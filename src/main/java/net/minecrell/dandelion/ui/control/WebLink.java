3b12b9c7f5a3a5540cf38b40063b06a9f9a42128
Minecrell <dev@minecrell.net>
2015-11-15 21:23:20 +0100

    Move and add new line

--- src/main/java/net/minecrell/dandelion/ui/control/WebLink.java
+++ src/main/java/net/minecrell/dandelion/ui/control/WebLink.java
@@ -19,7 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package net.minecrell.dandelion.ui.util;
+package net.minecrell.dandelion.ui.control;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
