d247c268a29e9a79f20ada1722b2db9b3f4bcd97
Minecrell <dev@minecrell.net>
2015-12-04 19:46:09 +0100

    Remove icon

--- src/main/java/net/minecrell/dandelion/Dandelion.java
+++ src/main/java/net/minecrell/dandelion/Dandelion.java
@@ -27,7 +27,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Dandelion extends Application {
@@ -69,7 +68,6 @@ public class Dandelion extends Application {
         scene.getStylesheets().add("css/dandelion.css");
 
         primaryStage.setTitle(NAME);
-        primaryStage.getIcons().add(new Image("img/dandelion.png"));
 
         primaryStage.setScene(scene);
         primaryStage.show();
