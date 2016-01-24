4ed1a4c09f10822b1e24fa3bec31e56ebf6f4c60
Minecrell <dev@minecrell.net>
2016-01-24 12:44:14 +0100

    Add ugly temporary icon so there is one at least

--- src/main/java/net/minecrell/dandelion/Dandelion.java
+++ src/main/java/net/minecrell/dandelion/Dandelion.java
@@ -27,6 +27,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Dandelion extends Application {
@@ -68,6 +69,7 @@ public class Dandelion extends Application {
         scene.getStylesheets().add("css/dandelion.css");
 
         primaryStage.setTitle(NAME);
+        primaryStage.getIcons().add(new Image("img/dandelion.png"));
 
         primaryStage.setScene(scene);
         primaryStage.show();
